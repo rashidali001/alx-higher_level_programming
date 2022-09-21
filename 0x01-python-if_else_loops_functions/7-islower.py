6866991002feecd9e6b63b57df104dcc6aa7b861
rashidali001 <rashidestonian@gmail.com>
2022-09-21 07:16:57 -0700

    0x01

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     numerical_value = ord(c)
-    if numerical_value >= 97 and numerical_value <= 123:
-        return true
+    if (numerical_value >= 97) and (numerical_value <= 123):
+        return True
     else:
-        return false
+        return False
