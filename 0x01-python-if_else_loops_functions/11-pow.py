03141db4413b5da27e458790dead670ee0ab0390
rashidali001 <rashidestonian@gmail.com>
2022-09-21 14:37:08 -0700

    0x01

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b < 0:
+        return 1 / pow(a, -b)
+    if b == 0:
+        return 1
+    i = 1
+    product = 1
+    while i <= b:
+        product = product * a
+        i += 1
+    return product
+
+
+print(pow(100, -2))
