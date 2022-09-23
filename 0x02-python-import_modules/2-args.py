f205e2ffdec2cc2ae63d9606406bfe36387f9ee5
rashidali001 <rashidestonian@gmail.com>
2022-09-22 18:53:51 -0700

    0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 import sys
+
 arg = len(sys.argv) - 1
-print('{} arguments:'.format(arg))
+
+if arg == 0:
+    print('{} arguments.'.format(arg))
+
+if arg == 1:
+    print('{} argument:'.format(arg))
+
+if arg > 1:
+    print('{} arguments:'.format(arg))
+
 i = 1
+
 while(i <= arg):
     print('{}: {}'.format(i, sys.argv[i]))
     i += 1
