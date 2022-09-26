734697d09a14b004b0fb743f770a7082aad3ab81
rashidali001 <rashidestonian@gmail.com>
2022-09-26 22:03:25 +0300

    Lists, Tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,3 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             print("{:d}".format(i[j]), end=" " if j < len(i) - 1 else "\n")
-
-    if len(matrix[0]) == 0:
-        print("{}".format(""))
