4a0eb9ffcebb99bf005a864a0cfcfd9a33d73c4f
rashidali001 <rashidestonian@gmail.com>
2022-09-26 08:22:09 -0700

    Lists, Tuples

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+def print_reversed_list_integer(my_list=[]):
+    reversed_list = []
+    length = len(my_list) - 1
+    for i in range(5):
+        reversed_list.append(my_list[length])
+        length -= 1
+    for i in reversed_list:
+        print("{}".format(i))
