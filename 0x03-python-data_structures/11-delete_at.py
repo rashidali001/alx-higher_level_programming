39d457ed669a94c8b155bd9d526c193af1bc633c
rashidali001 <rashidestonian@gmail.com>
2022-09-27 23:49:23 +0300

    Lists, Tuples

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,10 +4,3 @@ def delete_at(my_list=[], idx=0):
         del my_list[idx]
     return my_list
 
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
