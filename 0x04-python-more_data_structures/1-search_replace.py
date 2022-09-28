460ff5148729da64078e15ad2b1e477162da4712
rashidali001 <rashidestonian@gmail.com>
2022-09-28 23:44:27 +0300

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    list_a = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            list_a.append(replace)
+        else:
+            list_a.append(my_list[i])
+    return list_a
