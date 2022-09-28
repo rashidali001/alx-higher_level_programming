2068c74a6eda539e2052228ca0acb94720e143f0
rashidali001 <rashidestonian@gmail.com>
2022-09-29 01:11:58 +0300

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyton3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    score = 0
+    for i in a_dictionary:
+        if score < a_dictionary[i]:
+            score = a_dictionary[i]
+    return score
