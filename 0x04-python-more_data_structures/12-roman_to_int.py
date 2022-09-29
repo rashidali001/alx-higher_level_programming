439f14026a4e196ba8866837f1ef3b5e8f9ccb37
rashidali001 <rashidestonian@gmail.com>
2022-09-29 03:41:37 +0300

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
-    roman_string = roman_string.upper()
+    if roman_string.islower():
+        return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i in roman_string:
         if i not in roman:
