5a0f6adfc8d469b2a2561f06dfb43868aa72b369
rashidali001 <rashidestonian@gmail.com>
2022-10-12 01:24:49 +0300

    Pyhton exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,7 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             division.append(0)
-        except ArithmeticError:
+        except ValueError:
             division.append(0)
         finally:
             pass
