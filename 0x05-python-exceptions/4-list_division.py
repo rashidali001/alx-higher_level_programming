2bd5ad3a445668000967c14bdc5566c151c06ffb
rashidali001 <rashidestonian@gmail.com>
2022-10-12 01:40:46 +0300

    Pyhton exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,23 +4,19 @@ def list_division(my_list_1, my_list_2, list_length):
     division = []
     i = 0
     while i < list_length:
+        value = 0
         try:
             value = my_list_1[i] / my_list_2[i]
             division.append(value)
         except ZeroDivisionError:
             print("division by 0")
-            division.append(0)
         except IndexError:
             print("Out of range")
-            division.append(0)
         except TypeError:
             print("wrong type")
-            division.append(0)
         except ValueError:
             print("division by 0")
-            division.append(0)
         finally:
-            continue
-
+            division.append(value)
         i += 1
     return division
