5f51f478b0357070f677c4b194614dc3777027c9
rashidali001 <rashidestonian@gmail.com>
2022-10-14 02:47:20 +0300

    python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-'''Task'''
+'''
+Task:
+    1 - private instance attribute
+    2 - pass default value on size if not provided
+    3 - size must be an integer else raise TypeError
+    4 - if size < 0 raise ValueError
+'''
 
 
 class Square:
+    '''Defines a class with private object attributes'''
     def __init__(self, size=0):
-        '''This is a class'''
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            print("size must be >= 0")
         self.__size = size
