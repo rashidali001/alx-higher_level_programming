6e72f8168663bc8f718931b62a2f697ed8cb01ec
rashidali001 <rashidestonian@gmail.com>
2022-10-26 01:45:43 +0300

    python input/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import os
-'''Task 0'''
+'''a function that reads a text file (UTF8) and prints it to stdout '''
 
 
 def read_file(filename=""):
-    ''' Function used for reading files'''
+    ''' reads a text file (UTF8) and prints it to stdout '''
     with open(filename, "r", encoding="UTF8") as myFile:
         read_data = myFile.read()
         print(read_data, end="")
-
     myFile.close()
