2610fe4e8c9e917be678af2884f29fea80c20ec0
rashidali001 <rashidestonian@gmail.com>
2022-10-26 02:53:30 +0300

    python input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-'''adds all arguments to a Python list, and then save them to a file'''
+""" script to add arguments to list and save to a file """
 
 
 import sys
 
-save_to_json_file = __import__('5-save_tojson_file.py').save_to_json_file
-load_from_json_file =\
+
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = \
     __import__('6-load_from_json_file.py').load_from_json_file
 
-filename = "add_item.json"
+filename = 'add_item.json'
 
 try:
-    mylist = load_from_json_file(filename)
+    list_data = load_from_json_file(filename)
 except Exception as e:
-    mylist = []
+    list_data = []
 
 for arg in sys.argv[1:]:
-    mylist.append(arg)
+    list_data.append(arg)
 
-save_to_json_file(mylist, filename)
+save_to_json_file(list_data, filename)
