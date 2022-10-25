7bfd5aa81915c05435bfbbc82f94c200307f5620
rashidali001 <rashidestonian@gmail.com>
2022-10-26 02:49:53 +0300

    python input/output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''adds all arguments to a Python list, and then save them to a file'''
+import sys
+
+save_to_json_file = __import__('5-save_tojson_file.py').save_to_json_file
+load_from_json_file =\
+    __import__('6-load_from_json_file.py').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    mylist = load_from_json_file(filename)
+except Exception as e:
+    mylist = []
+
+for arg in sys.argv[1:]:
+    mylist.append(arg)
+
+save_to_json_file(mylist, filename)
