843e49afe5d4a150fcd18e734aa794f5e5bd88b6
rashidali001 <rashidestonian@gmail.com>
2022-10-28 00:46:19 +0300

    Python inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Prints list in ascending order'''
+
+
+class Mylist:
+    ''''MyList class'''
+
+    def print_sorted(self):
+        '''Public instance method'''
+
+        print(sorted(self))
