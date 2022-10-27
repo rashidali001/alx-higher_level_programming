04c5f085f8bdb1250ff5ce8e3f4445e59afbbc9d
rashidali001 <rashidestonian@gmail.com>
2022-10-28 00:28:09 +0300

    Python inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Return all attributee and methods available in an object'''
+
+
+def lookup(obj):
+    '''Takes an object as input and returns its attributes and methods'''
+
+    return dir(obj)
