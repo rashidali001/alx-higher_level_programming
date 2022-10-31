19b73f15e8e87269ef5ab00ad4e5d6726c195249
rashidali001 <rashidestonian@gmail.com>
2022-11-01 00:55:20 +0300

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,8 @@ class Rectangle(Base):
     '''Rectangle class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''Initializing'''
+
         super(Rectangle, self).__init__(id)
         self.width = width
         self.height = height
@@ -20,32 +22,40 @@ class Rectangle(Base):
 
     @property
     def get_width(self):
+        '''getting width'''
         return self.__width
 
     @get_width.setter
     def set_width(self, value):
+        '''setting width'''
         self.__witdh = value
 
     @property
     def get_height(self):
+        '''getting height'''
         return self.__width
 
     @get_height.setter
     def set_height(self, value):
+        '''setting height'''
         self.__height = value
 
     @property
     def get_x(self):
+        '''getting x'''
         return self.__width
 
     @get_x.setter
     def set_x(self, value):
+        '''setting x'''
         self.__height = value
 
     @property
     def get_y(self):
+        '''getting y'''
         return self.__width
 
     @get_y.setter
     def set_y(self, value):
+        '''setting y'''
         self.__height = value
