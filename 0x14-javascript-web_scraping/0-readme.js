7eff9ae020950d0bf3e1f32f4a1970b741a87496
rashidali001 <rashidestonian@gmail.com>
2023-02-09 15:39:42 +0000

    Task 0: Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,8 @@ const arg1 = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(arg1, 'utf-8', (err, data) => {
-  if (err){
-	console.error(err);
+  if (err) {
+    console.error(err);
   }
   console.log(data);
 });
-
