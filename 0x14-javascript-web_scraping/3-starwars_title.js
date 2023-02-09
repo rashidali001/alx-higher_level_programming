65abf757fe7d54801d049dde264f65ad9ef73954
rashidali001 <rashidestonian@gmail.com>
2023-02-09 20:45:49 +0000

    Task 2: Status code

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,9 @@
 const id = process.argv[2];
 const request = require('request');
 
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, function (error, response, body) {
-  console.error(error);
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body) => {
+  if(error){
+    console.error(error);
+  }
   console.log(JSON.parse(body).title);
 });
