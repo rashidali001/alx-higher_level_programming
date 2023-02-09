1a71df815f30355139e85c6c01f8d4acf5776b3e
rashidali001 <rashidestonian@gmail.com>
2023-02-09 21:23:01 +0000

    Task 2: Status code

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const URL = process.argv[2];
+const path = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+
+request(`${URL}`, (error, response, body) => {
+  if (error){
+    console.error(error);
+  }
+  const content = body;
+  fs.writeFile(`${path}`, content, 'utf-8', (error) => {
+  if (error){
+    console.error(error);
+  }
+})});
