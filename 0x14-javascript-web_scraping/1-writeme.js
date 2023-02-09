266ec6b1c4af22e84778f54ae5cdba87585d082e
rashidali001 <rashidestonian@gmail.com>
2023-02-09 19:37:43 +0000

    Task 0: Readme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const path = process.argv[2];
+const insert = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(`${path}`, `${insert}`, 'utf-8', (err) => {
+  if (err){
+    console.error(err);
+  }
+});
